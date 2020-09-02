5ca6afd4103fa812491c3cbcd811a63c6feaca18
Kaic Melo <kaicmelosantos@gmail.com>
2020-09-01 21:22:12 -0300

    closing the front end the application in react

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,8 @@ import path from 'path';
 var app = express();
 
 app.use(express.json()); 
-app.use(routes);
 app.use(cors());
+app.use(routes);
 
 app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')))
 app.listen(3333);
