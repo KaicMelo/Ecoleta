cd762e9e0aa0d1f644033a8fdf40ac551bd58b23
Kaic Melo <kaicmelosantos@gmail.com>
2020-09-02 16:35:25 -0300

    alter seed create_items

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -3,11 +3,11 @@ import Knex from 'knex';
 export async  function seed(knex:Knex)
 {
     await knex("items").insert([
-        {title: 'Lampadas',image:'lampada.svg'},
+        {title: 'Lampadas',image:'lampadas.svg'},
         {title: 'Pilhas e baterias',image:'baterias.svg'},
         {title: 'Papéis e Papelão',image:'papeis-papelao.svg'},
         {title: 'Resíduos Eletrônicos',image:'eletronicos.svg'},
-        {title: 'Resíduos Orgnicos',image:'ôrganicos.svg'},
+        {title: 'Resíduos Orgnicos',image:'organicos.svg'},
         {title: 'Óleo de Cozinha',image:'oleo.svg'}
     ]);
 }
